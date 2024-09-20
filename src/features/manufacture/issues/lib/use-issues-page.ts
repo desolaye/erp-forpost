bc54911062f297052b85d2305b8b06543c184921
desolaye <ex4to@icloud.com>
2024-09-20 14:12:05 +0700

    chore(ERP-9): issues page

--- src/features/manufacture/issues/lib/use-issues-page.ts
+++ src/features/manufacture/issues/lib/use-issues-page.ts
@@ -1,66 +1,25 @@
-import { useState } from 'react'
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { useQuery } from '@tanstack/react-query'
 
 import { usePagination } from '@/shared/lib/use-pagination'
-import {
-  putIssueClose,
-  putIssueLaunch,
-  getIssuesByManufacture,
-} from '@/entities/manufacture'
+import { getIssuesByManufacture } from '@/entities/manufacture'
 
 export const useIssuesPage = (manufactureId: string) => {
   const { page, setPage } = usePagination(8)
-  const [selectedIds, setSelectedIds] = useState<string[]>([])
-
-  const queryClient = useQueryClient()
 
   const { data, isPending } = useQuery({
     queryFn: () => getIssuesByManufacture(manufactureId),
     queryKey: ['issues_all', manufactureId],
   })
 
-  const { mutateAsync: mutateLaunch, isPending: isLaunchPending } = useMutation({
-    mutationFn: (id: string) => putIssueLaunch(id),
-  })
-
-  const { mutateAsync: mutateClose, isPending: isClosePending } = useMutation({
-    mutationFn: (id: string) => putIssueClose(id),
-  })
-
-  const selectId = (id: string) => {
-    const idx = selectedIds.findIndex((v) => v === id)
-
-    setSelectedIds((prev) =>
-      idx === -1 ? [...prev, id] : selectedIds.filter((v) => v !== id),
-    )
-  }
-
-  const launchAll = () => {
-    Promise.all(selectedIds.map((v) => mutateLaunch(v))).then(() => {
-      queryClient.invalidateQueries({ queryKey: ['issues_all', manufactureId] })
-    })
-  }
-
-  const closeAll = () => {
-    Promise.all(selectedIds.map((v) => mutateClose(v))).then(() => {
-      queryClient.invalidateQueries({ queryKey: ['issues_all', manufactureId] })
-    })
-  }
-
   return {
     values: {
       issues: data,
       totalCount: 1,
       page,
-      selectedIds,
       isPending,
-      isPendingAction: isLaunchPending || isClosePending,
     },
     handlers: {
       setPage,
-      selectId,
-      launchAll,
-      closeAll,
     },
   }
 }
--- src/pages/manufacture/issues/ui/manufacture-issues.tsx
+++ src/pages/manufacture/issues/ui/manufacture-issues.tsx
@@ -1,12 +1,10 @@
 import { useParams } from '@tanstack/react-router'
 
 import { Table } from '@/shared/ui/table'
-import { ToolMenu } from '@/shared/ui/tool-menu'
 
 import { useIssuesPage } from '@/features/manufacture/issues'
 import { PageWrapper } from '@/widgets/layouts/page-wrapper'
 
-import { issuesToolMenu } from '../utils/issues-tool-menu'
 import { IssuesTableHead } from './components/issues-table-head'
 import { IssuesTableBody } from './components/issues-table-body'
 
@@ -16,16 +14,8 @@ export const ManufactureIssues = () => {
 
   return (
     <PageWrapper title="Производственные задачи">
-      <ToolMenu
-        tools={issuesToolMenu({
-          selectedLength: values.selectedIds.length,
-          onComplete: handlers.closeAll,
-          onLaunch: handlers.launchAll,
-        })}
-      />
-
       <Table
-        body={<IssuesTableBody onCheck={handlers.selectId} data={values.issues} />}
+        body={<IssuesTableBody data={values.issues} />}
         header={<IssuesTableHead />}
         isPending={values.isPending}
         page={values.page}
