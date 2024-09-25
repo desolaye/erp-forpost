c5670bd437eaa55a8f92cffa642383a5bc94a92e
desolaye <ex4to@icloud.com>
2024-09-25 11:21:50 +0700

    chore(ERP-7): 401 status handle

---
+++ src/widgets/layouts/erp-layout/lib/use-erp-layout.ts
@@ -0,0 +1,45 @@
+import { useNavigate } from '@tanstack/react-router'
+import { useContext } from 'react'
+import Cookies from 'js-cookie'
+
+import { routesPath } from '@/shared/config/routes-path.config'
+import { publicApi } from '@/shared/api/public-api.config'
+import { APP_VARS } from '@/shared/config/app-variables.config'
+
+import { SessionContext, useLocalSession } from '@/entities/session'
+
+export const useErpLayout = () => {
+  const navigate = useNavigate()
+
+  const cookie = Cookies.get(APP_VARS.TOKEN)
+  const sessionContext = useContext(SessionContext)
+  const { getLocalSession, setLocalSession } = useLocalSession()
+
+  const onAuthFail = () => {
+    navigate({ to: routesPath.login() })
+    setLocalSession(null)
+  }
+
+  if (!cookie) {
+    onAuthFail()
+  } else {
+    publicApi.interceptors.request.use((config) => {
+      config.headers.Authorization = `Bearer ${cookie}`
+      return config
+    })
+
+    publicApi.interceptors.response.use(
+      (res) => res,
+      (err) => {
+        if (err.response.status === 401) onAuthFail()
+        return err
+      },
+    )
+
+    const session = getLocalSession()
+
+    if (!sessionContext.session && session) {
+      sessionContext.setSession({ ...session })
+    }
+  }
+}
--- src/widgets/layouts/erp-layout/ui/erp-layout.tsx
+++ src/widgets/layouts/erp-layout/ui/erp-layout.tsx
@@ -1,38 +1,12 @@
-import { Outlet, useNavigate } from '@tanstack/react-router'
-import Cookies from 'js-cookie'
-import { useContext } from 'react'
+import { Outlet } from '@tanstack/react-router'
 
-import { routesPath } from '@/shared/config/routes-path.config'
-import { publicApi } from '@/shared/api/public-api.config'
-import { APP_VARS } from '@/shared/config/app-variables.config'
-
-import { SessionContext, useLocalSession } from '@/entities/session'
 import { NavMenu } from '@/features/nav-menu'
 
 import cls from './erp-layout.module.scss'
+import { useErpLayout } from '../lib/use-erp-layout'
 
 export const ErpLayout = () => {
-  const navigate = useNavigate()
-
-  const cookie = Cookies.get(APP_VARS.TOKEN)
-  const sessionContext = useContext(SessionContext)
-  const { getLocalSession, setLocalSession } = useLocalSession()
-
-  if (!cookie) {
-    navigate({ to: routesPath.login() })
-    setLocalSession(null)
-  } else {
-    publicApi.interceptors.request.use((config) => {
-      config.headers.Authorization = `Bearer ${cookie}`
-      return config
-    })
-
-    const session = getLocalSession()
-
-    if (!sessionContext.session && session) {
-      sessionContext.setSession({ ...session })
-    }
-  }
+  useErpLayout()
 
   return (
     <article className={cls.erp_layout}>
